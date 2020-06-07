dee10da67458edf8144a73b0134b8163b0bdc997
nguardadov <nguardadov@gmail.com>
2020-06-07 15:13:15 -0600

    add to sedders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,5 +24,19 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'address' => $faker->address(),
+        'phone' => $faker->e164PhoneNumber(),
+        'dni' => $faker->randomNumber(8,true),
+        'role'=>$faker->randomElement(['patient','doctor'])                      
+        //
+
+        /**
+         * 
+         *  $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('dni');
+            $table->string('role'); // admin patient doctor
+         * 
+         */
     ];
 });
